567e7c2cbd8c41b9939aceab8567d62816ca8a2a
Beloved-vince <vinceoludare@gmail.com>
2023-04-17 17:23:36 +0100

    update

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,22 @@
+// Using const to instantiate variables
+function taskFirst() {
+    // const message = "Hello, world!";
+    const PI = 3.14159;
+    const isTrue = true;
+  
+    console.log(message);
+    console.log(PI);
+    console.log(isTrue);
+  }
+  
+  // Using let to instantiate variables
+  function taskNext() {
+    let count = 0;
+    let name = "John Doe";
+    let isEnabled = false;
+  
+    console.log(count);
+    console.log(name);
+    console.log(isEnabled);
+  }
+  
