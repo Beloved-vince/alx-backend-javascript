8e483cd58200dfc502bb34c132fae9318d7996d5
Beloved-vince <vinceoludare@gmail.com>
2023-04-17 17:38:00 +0100

    const, let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,22 +1,15 @@
-// Using const to instantiate variables
-function taskFirst() {
-    // const message = "Hello, world!";
-    const PI = 3.14159;
-    const isTrue = true;
-  
-    console.log(message);
-    console.log(PI);
-    console.log(isTrue);
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
   }
   
-  // Using let to instantiate variables
-  function taskNext() {
-    let count = 0;
-    let name = "John Doe";
-    let isEnabled = false;
-  
-    console.log(count);
-    console.log(name);
-    console.log(isEnabled);
+  export function getLast() {
+    return ' is okay';
   }
   
+  export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+  
+    return combination;
+  }
