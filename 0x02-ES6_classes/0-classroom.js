4e173ef6ee5d9d543d0526b0ecb19407e11c09fc
Beloved-vince <vinceoludare@gmail.com>
2023-04-20 16:37:52 +0100

    class

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * class representtation
+ */
+export default class ClassRoom {
+    /**
+     *Insatiate new class @see {@link ClassRoom}.
+     * @param {Number} maxStudentsSize - Return the total number of
+     * student in a class
+     */
+    constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
+    }
+  }
