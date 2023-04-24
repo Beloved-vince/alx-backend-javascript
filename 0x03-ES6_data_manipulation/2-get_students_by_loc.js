768dd5357da1ef505dbf9658c56a50682b132d8d
Beloved-vince <vinceoludare@gmail.com>
2023-04-24 20:34:13 +0100

    data manipulation

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,16 @@
+/**
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students -
+* @param {String} city - The location.
+* @author Beloved-Vince
+* @returns
+*/
+export default function getStudentsByLocation(students, city) {
+ if (students instanceof Array) {
+   return students.filter((student) => student.location === city);
+ }
+ return [];
+}
