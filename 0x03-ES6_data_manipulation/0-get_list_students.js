62650f199c8dd31d3fc9386e8edc572615335fc4
Beloved-vince <vinceoludare@gmail.com>
2023-04-24 20:26:38 +0100

    data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * This function retrieve list of student
+ * @author Bezaleel Olakunori 
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+    return [
+      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+  }
