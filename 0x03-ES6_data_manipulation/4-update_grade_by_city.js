90fac442cce0adc65077c7bdd4b1a219e9036250
Beloved-vince <vinceoludare@gmail.com>
2023-04-24 20:38:39 +0100

    data manipulation

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students 
+* @param {*} city 
+* 
+* @author Beloved
+* @returns {{id: Number, firstName: String, location: String}[]}
+*/
+export default function updateStudentGradeByCity(students, city, newGrades) {
+ const defaultGrade = { grade: 'N/A' };
+
+ if (students instanceof Array) {
+   return students
+     .filter((student) => student.location === city)
+     .map((student) => ({
+       id: student.id,
+       firstName: student.firstName,
+       location: student.location,
+       grade: (newGrades
+         .filter((grade) => grade.studentId === student.id)
+         .pop() || defaultGrade).grade,
+     }));
+ }
+ return [];
+}
