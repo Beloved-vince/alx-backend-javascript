565fcfe881b1a1beda72a52018432b78d921adea
Beloved-vince <vinceoludare@gmail.com>
2023-06-16 13:15:49 +0100

    node basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Adeyemo Oludare <https://github.com/Beloved-vince>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
